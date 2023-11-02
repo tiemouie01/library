7bf0f8b5fe9d3a99597c0fd1902894dd53ce836d
Timothy Miamba <timal0361@gmail.com>
2023-11-02 15:17:59 +0200

    Simplify viewLibrary function

--- index.js
+++ index.js
@@ -35,41 +35,46 @@ function addBookToLibrary() {
     myLibrary.push(book);
 }
 
+function createRows(table,title,author,pages,read) {
+    const row = document.createElement('tr');
+    table.appendChild(row);
+
+    const cells = [];
+    for(let i = 0; i < 4; i++) {
+        let cell;
+        if (title === 'Title'){
+            cell = document.createElement('th');
+        } else {
+            cell = document.createElement('td');
+        }
+        cells.push(cell);
+        row.appendChild(cell);
+    }
+    cells[0].textContent = title;
+    cells[1].textContent = author;
+    cells[2].textContent = pages;
+    cells[3].textContent = read;
+}
+
 function viewLibrary() {
     const body = document.body;
     const table = document.createElement('table');
     body.appendChild(table);
-    const headerRow = document.createElement('tr');
-    table.appendChild(headerRow);
-    const headers = [];
-    for(let i = 0; i < 4; i++) {
-        const cell = document.createElement('th');
-        headers.push(cell);
-        headerRow.appendChild(cell);
-    }
-    headers[0].textContent = 'Title';
-    headers[1].textContent = 'Author';
-    headers[2].textContent = 'Number of Pages';
-    headers[3].textContent = 'Read Status';
+    createRows(table,'Title','Author','Pages','Read');
     
     for(let i = 0; i < myLibrary.length; i++) {
         const book = myLibrary[i];
-        const row = document.createElement('tr');
-        table.appendChild(row);
-
-        const cells = [];
-        for(let i = 0; i < 4; i++) {
-            const cell = document.createElement('td');
-            cells.push(cell);
-            row.appendChild(cell);
-        }
-        cells[0].textContent = book.title;
-        cells[1].textContent = book.author;
-        cells[2].textContent = book.pages;
-        cells[3].textContent = book.read;
+        createRows(table,book.title,book.author,book.pages,book.read);
     }
 }
-// const book1 = new Book('Sapiens','Yuval Noah Harari',380,'finished reading');
-// const book2 = new Book('Why We Sleep','Matt Walker',260,'finished reading');
-// const book3 = new Book('Astrophysics for People in a Hurry','Neil Tyson',120,'still reading');
-// const book4 = new Book('Rationality','Steven Pinker',330,'not yet read');
+
+const book1 = new Book('Sapiens','Yuval Noah Harari',380,'finished reading');
+const book2 = new Book('Why We Sleep','Matt Walker',260,'finished reading');
+const book3 = new Book('Astrophysics for People in a Hurry','Neil Tyson',120,'still reading');
+const book4 = new Book('Rationality','Steven Pinker',330,'not yet read');
+myLibrary.push(book1);
+myLibrary.push(book2);
+myLibrary.push(book3);
+myLibrary.push(book4);
+
+viewLibrary();
