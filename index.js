37855d09032b78ec9bdd8a05ef96462dcd7b70ac
Timothy Miamba <timal0361@gmail.com>
2023-11-02 07:25:22 +0200

    Add function that creates and stores Book objects in a library array

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -7,3 +9,30 @@ function Book(title, author, pages, read) {
         return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
     }
 }
+
+function addBookToLibrary() {
+    const title = prompt('Enter the title of the book:');
+    const author = prompt('Enter the fullname of the author:');
+    const pages = Number(prompt('Enter the number of pages in the book:'));
+
+    let read;
+    const status = Number(prompt('Have you read the book yet?\n1. Not read yet\n2. Still reading\n3.Finished reading'));
+    switch(status) {
+        case 1:
+            read = 'not read yet';
+            break;
+        case 2:
+            read = 'still reading';
+            break;
+        case 3:
+            read = 'finished reading';
+            break;
+        default:
+            alert('You entered an incorrect option.');
+            break;
+    }
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+addBookToLibrary();
